3913537a550f179e7fbf5bf788fcf0fa4b9aaced
ishtiyak98 <rifatishtiyak@gmail.com>
2022-10-05 22:53:25 +0600

    uploaded

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Demo\DemoController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::controller(DemoController::class)->group(function(){
+    Route::get('/about', 'AboutMethod')->name('about.page')->middleware('check');
+    Route::get('/contact', 'ContactMethod')->name('contact.page');
+});
